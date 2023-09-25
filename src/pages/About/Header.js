df0b568635efb5e5e56e7a8c488d9d27ce8b60b2
Vinicius <karvvinicius@gmail.com>
2023-09-25 22:49:35 +0100

    add

--- src/pages/About/Header.js
+++ src/pages/About/Header.js
@@ -1,6 +1,10 @@
 import ImgbackgroudTop from '../../components/HOMESite.png';
 import ButtonMaster from '../../components/buttonMaster';
 import './Header.css';
+import Icon1 from '../../components/icons/ethics.png';
+import Icon2 from '../../components/icons/quality.png';
+import Icon3 from '../../components/icons/icons8-handshake-64.png';
+
 
 function Header() {
     return (
@@ -16,13 +20,25 @@ function Header() {
         </div>
         <div className='ContainerCondulta'>
             <div>
+                <img src={Icon3} alt="Icons" />
                 <h5>CONFIANÇA</h5>
+                <p>
+                  A confiança é o alicerce das relações humanas, construída com honestidade e mantida com lealdade.
+                </p>
             </div>
             <div>
+                <img src={Icon2} alt="Icons" />
                 <h5>CREDIBILIDADE</h5>
+                <p>
+                  A credibilidade é um ativo valioso, conquistado com integridade e consistência ao longo do tempo
+                </p>
             </div>
             <div>
+                <img src={Icon1} alt="Icons" />
                 <h5>ÉTICA</h5>
+                <p>
+                  A ética é o farol que guia nossas decisões, orientando-nos na busca pelo que é correto e justo.
+                </p>
             </div>
         </div>
         <div className='CampoSection'>
