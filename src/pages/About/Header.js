ac49a25274e9168c1865e7e6ac4f81d6cc2fbedc
Vinicius <karvvinicius@gmail.com>
2023-09-26 22:40:22 +0100

    add CampoInfo

--- src/pages/About/Header.js
+++ src/pages/About/Header.js
@@ -5,7 +5,8 @@ import Icon1 from '../../components/icons/ethics.png';
 import Icon2 from '../../components/icons/quality.png';
 import Icon3 from '../../components/icons/icons8-handshake-64.png';
 
-
+import ImgCera from '../../components/img/ServiceDepilacaoCera.jpg';
+import ImgLaser from '../../components/img/ServiceDepilacaoLaser.jpg';
 function Header() {
     return (
       <div>
@@ -18,7 +19,9 @@ function Header() {
                   nameclass= "buttonNormal"
                 />
         </div>
-        <div className='ContainerCondulta'>
+        <div className='CampoCondulta'>
+          <div className='TabCondulta'></div>
+          <div className='ContainerCondulta'>
             <div>
                 <img src={Icon3} alt="Icons" />
                 <h5>CONFIANÇA</h5>
@@ -41,8 +44,26 @@ function Header() {
                 </p>
             </div>
         </div>
+        </div>
+        
         <div className='CampoSection'>
             <h2>Descubra o Seu Refúgio<br/>de Beleza e Bem-Estar</h2>
+            <div className='CampoHeaderInfo'>
+              <div className='campInfo'>
+                <img src={ImgCera} alt="Img Cera" />
+                <div>
+                  <h4>Depilação Profissional</h4>
+                  <p>Diga adeus aos pelos indesejados com a nossa depilação profissional a cera. Nossos especialistas experientes garantem uma experiência suave e eficaz, deixando sua pele irresistivelmente macia.</p>
+                </div>                
+              </div>
+              <div className='campInfo'>
+                <img src={ImgCera} alt="Img Cera" />
+                <div>
+                  <h4>Depilação Profissional</h4>
+                  <p>Diga adeus aos pelos indesejados com a nossa depilação profissional a cera. Nossos especialistas experientes garantem uma experiência suave e eficaz, deixando sua pele irresistivelmente macia.</p>
+                </div>                
+              </div>
+            </div>
         </div>
       </div>
     );
