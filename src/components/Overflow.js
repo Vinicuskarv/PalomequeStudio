4f3577d8fdca90f7451135f06d4ad6ea59442312
Vinicius <karvvinicius@gmail.com>
2023-11-09 22:32:57 +0000

    add

--- src/components/Overflow.js
+++ src/components/Overflow.js
@@ -3,6 +3,12 @@ import Card from './cardOverflow';
 import './Overflow.css';
 import iconArrow from './icons/icons8-arrow-30.png';
 
+import img1 from '../components/img/ServiceDepilacaoCera.jpg';
+import img2 from '../components/img/ServiceDepilacaoLaser.jpg';
+import img3 from '../components/img/ServiceManicure.jpg';
+import img4 from '../components/img/ServiceMassagem.jpg';
+import img5 from '../components/img/ServiceSobrancelhas.jpg';
+
 const App = () => {
   const containerRef = useRef(null);
 
@@ -10,7 +16,7 @@ const App = () => {
     const container = containerRef.current;
     if (container) {
       container.scrollBy({
-        left: -300,
+        left: -250,
         behavior: 'smooth',
       });
     }
@@ -20,7 +26,7 @@ const App = () => {
     const container = containerRef.current;
     if (container) {
       container.scrollBy({
-        left: 300,
+        left: 250,
         behavior: 'smooth',
       });
     }
@@ -32,14 +38,12 @@ const App = () => {
         <img src={iconArrow}  alt="icons icon Arrow" />
       </button>
       <div className="cards" ref={containerRef}>
-        <Card title="Card 1" content="Conteúdo do Card 1" />
-        <Card title="Card 2" content="Conteúdo do Card 2" />
-        <Card title="Card 3" content="Conteúdo do Card 3" />
-        <Card title="Card 3" content="Conteúdo do Card 3" />
-        <Card title="Card 3" content="Conteúdo do Card 3" />
-        <Card title="Card 3" content="Conteúdo do Card 3" />
-        <Card title="Card 3" content="Conteúdo do Card 3" />
-        <Card title="Card 3" content="Conteúdo do Card 3" />
+        <Card title="Massagem Mãos e Pês" content="Conteúdo do Card 1" Img={img4} />
+        <Card title="Laser" content="Conteúdo do Card 2" Img={img2} />
+        <Card title="Manicure" content="Reparação verniz em gel, unhas em gel manicure, Gel na tips" Img={img3} />
+        <Card title="Cera" content="Conteúdo do Card 4" Img={img1} />
+        <Card title="Sobrancelhas" content="Conteúdo do Card 5" Img={img5} />
+
       </div>
       <button className="scroll-button right" onClick={scrollRight}>
         <img src={iconArrow}  alt="icons icon Arrow" />
--- src/components/cardOverflow.js
+++ src/components/cardOverflow.js
@@ -1,6 +1,7 @@
-const Card = ({ title, content }) => {
+const Card = ({ title, content, Img }) => {
     return (
       <div className="card">
+        <img src={Img}  alt="Img do card" />
         <h4>{title}</h4>
         <p>{content}</p>
       </div>
